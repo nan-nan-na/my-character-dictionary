07be54912be76aa9c0f96423f35f6b1662223c0d
nana-y <nana-y@example.com>
2019-04-04 16:10:11 +0900

    コード整形の改善

--- my-character-dictionary/src/components/Navbar.vue
+++ my-character-dictionary/src/components/Navbar.vue
@@ -2,10 +2,17 @@
   <div id="navbar">
     <b-navbar toggleable variant="info" type="light">
       <b-navbar-brand href="#">
-        <img src="../assets/lower_m.png" class="d-inline-block align-top" alt="【M】">
+        <img
+          src="../assets/lower_m.png"
+          class="d-inline-block align-top"
+          alt="【M】"
+        />
         Myキャラ図鑑
       </b-navbar-brand>
-      <b-navbar-toggle target="nav-text-collapse" label="メニュー"></b-navbar-toggle>
+      <b-navbar-toggle
+        target="nav-text-collapse"
+        label="メニュー"
+      ></b-navbar-toggle>
 
       <b-collapse id="nav-text-collapse" is-nav>
         <b-navbar-nav>
