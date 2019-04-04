4d4da62ff90355a26b7b5b2876b11d2fbfed2abe
nana-y <nana-y@example.com>
2019-04-04 16:10:31 +0900

    カードの追加（第1段階）

--- my-character-dictionary/src/App.vue
+++ my-character-dictionary/src/App.vue
@@ -1,42 +1,21 @@
 <template>
   <div id="app">
-    <Navbar/>
-    <b-card>
-      <b-media>
-        <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder"></b-img>
-
-        <h5 class="mt-0">Media Title</h5>
-        <p>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
-          Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-          ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </p>
-        <p>
-          Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </p>
-
-        <b-media>
-          <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder"></b-img>
-
-          <h5 class="mt-0">Nested Media</h5>
-          <p class="mb-0">
-            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in
-            faucibus.
-          </p>
-        </b-media>
-      </b-media>
-    </b-card>
+    <Navbar />
+    <Card />
+    <!-- v-forでデータ数分だけカードを並べる -->
+    <Card />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Navbar from "@/components/Navbar.vue";
+import Card from "@/components/Card.vue";
 
 @Component({
   components: {
-    Navbar
+    Navbar,
+    Card
   }
 })
 export default class App extends Vue {}
---
+++ my-character-dictionary/src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-50">
+    <b-card class="m-3">
+      <b-media no-body>
+        <b-media-aside>
+          <b-img-lazy
+            thumbnail
+            blank
+            blank-color="#ccc"
+            width="64"
+            height="200"
+            :src="getImgUrl()"
+            alt="キャラクター画像"
+          ></b-img-lazy>
+        </b-media-aside>
+        <b-media-body class="ml-3">
+          <h5 class="mt-0">charactpr name ( age ) [ gender ]</h5>
+          <p>job</p>
+          <p>supplement</p>
+          <p>system</p>
+          <p>scenario</p>
+        </b-media-body>
+      </b-media>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Card extends Vue {
+  private getImgUrl() {
+    return require("../assets/lower_m.png");
+  }
+}
+</script>
