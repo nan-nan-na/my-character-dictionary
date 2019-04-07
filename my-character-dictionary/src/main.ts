9411ea2421bd38287434f3d8378056e4eb83247c
nana-y <nana-y@example.com>
2019-04-07 11:06:56 +0900

    firebaseの設定をしてみたけど、initが上手く行かなかった。後回し

--- my-character-dictionary/src/main.ts
+++ my-character-dictionary/src/main.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import * as firebase from "firebase/app";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -8,6 +9,20 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+// firebase設定値・初期化処理
+const config = {
+  apiKey: "AIzaSyDrtoDH-OO8ILdnwEqvJaULHG1dXqJ8Zvo",
+  authDomain: "my-char-dictionary.firebaseapp.com",
+  databaseURL: "https://my-char-dictionary.firebaseio.com",
+  projectId: "my-char-dictionary",
+  storageBucket: "my-char-dictionary.appspot.com",
+  messagingSenderId: "102427001734"
+};
+firebase.initializeApp(config);
+
+// storeの初期化
+store.dispatch("init");
 
 new Vue({
   router,
